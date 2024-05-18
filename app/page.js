d79597ab491f96d3c0012ef33bf0d28ffb3acbff
esther <estherpuihh@gmail.com>
2024-05-19 00:37:01 +0800

    fix some bugs + create action files

--- app/page.js
+++ app/page.js
@@ -35,7 +35,7 @@ export default function Home() {
             <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>Esther Pui</h2>
             <h3 className='text-2xl py-2 md:text-3xl dark:text-gray-200'>Developer and designer.</h3>
             <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-gray-300'>
-              Freelancer providing services for programming design needs. Join me below and let's get cracking!
+              Freelancer providing services for programming design needs. Join me below and let&apos;s get cracking!
             </p>
           </div>
           <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-300'>
@@ -50,7 +50,7 @@ export default function Home() {
             </a>
           </div>
           <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
-            <Image src={bee} />
+            <Image src={bee} alt=""/>
           </div>
         </section>
 
@@ -58,7 +58,7 @@ export default function Home() {
           <div>
             <h3 className='text-3xl py-1 dark:text-gray-100'>Services I offer</h3>
             <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-300'>
-              Since the beginning of my journey as a freelancer designer and developer, I've done remote work for
+              Since the beginning of my journey as a freelancer designer and developer, I&apos;ve done remote work for
               <span className='text-teal-500'> agencies </span>
               consulted for <span className='text-teal-500'> startups </span> and collaborated with talented people to create digital products for both business and consumer use.
             </p>
@@ -69,7 +69,7 @@ export default function Home() {
           <div className='lg:flex gap-10'>
             <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-50'>
               <div className="flex justify-center">
-                <Image src={design} width={100} height={100}/>
+                <Image src={design} width={100} height={100} alt=""/>
               </div>
               <h3 className='text-lg front-medium pt-8 pb-2'>Beautiful Designs</h3>
               <p className='py-2'>
@@ -84,7 +84,7 @@ export default function Home() {
 
             <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-50'>
               <div className="flex justify-center">
-                <Image src={design} width={100} height={100}/>
+                <Image src={design} width={100} height={100} alt=""/>
               </div>
               <h3 className='text-lg front-medium pt-8 pb-2'>Beautiful Designs</h3>
               <p className='py-2'>
@@ -99,7 +99,7 @@ export default function Home() {
 
             <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-50'>
               <div className="flex justify-center">
-                <Image src={design} width={100} height={100}/>
+                <Image src={design} width={100} height={100} alt=""/>
               </div>
               <h3 className='text-lg front-medium pt-8 pb-2'>Beautiful Designs</h3>
               <p className='py-2'>
@@ -118,7 +118,7 @@ export default function Home() {
           <div>
             <h3 className='text-3xl py-1 dark:text-gray-100'>Portfolio</h3>
             <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-300'>
-              Since the beginning of my journey as a freelancer designer and developer, I've done remote work for
+              Since the beginning of my journey as a freelancer designer and developer, I&apos;ve done remote work for
               <span className='text-teal-500'> agencies </span>
               consulted for <span className='text-teal-500'> startups </span> and collaborated with talented people to create digital products for both business and consumer use.
             </p>
@@ -128,28 +128,28 @@ export default function Home() {
           </div>
           <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
             <div className='basis-1/3 flex-1'>
-              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"'/>
+              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"' alt=""/>
             </div>
             <div className='basis-1/3 flex-1'>
-              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"'/>
+              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"' alt=""/>
             </div>
             <div className='basis-1/3 flex-1'>
-              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"'/>
+              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"' alt=""/>
             </div>
             <div className='basis-1/3 flex-1'>
-              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"'/>
+              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"' alt=""/>
             </div>
             <div className='basis-1/3 flex-1'>
-              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"'/>
+              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"' alt=""/>
             </div>
             <div className='basis-1/3 flex-1'>
-              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"'/>
+              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"' alt=""/>
             </div>
             <div className='basis-1/3 flex-1'>
-              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"'/>
+              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"' alt=""/>
             </div>
             <div className='basis-1/3 flex-1'>
-              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"'/>
+              <Image src={render} className='rounded-lg object-cover width={"100"} height={"100%"} layout="responsive"' alt=""/>
             </div>
           </div>
         </section>
