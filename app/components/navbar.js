cabe23127e81f93a786a22a90c6b652d281e233a
esther <estherpuihh@gmail.com>
2024-05-22 06:58:38 +0800

    mobile responsive navbar

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -1,6 +1,14 @@
+'use client';
 import Link from 'next/link';
+import { useState } from 'react';
+
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
         <nav className='py-10 mb-12 flex justify-between'>
@@ -8,16 +16,24 @@ export default function Navbar() {
               <Link href="/">esther pui.</Link>
             </h1>
            
-            <ul className='sm:flex items-center hidden'>
-              <li>
-                <a className=' text-the-red mr-6 hover:text-the-off-white hover:bg-the-red transition duration-300' href="#">download resume</a>
-              </li>
-              <li>
-              <a className=' text-the-red mr-6 hover:text-the-off-white hover:bg-the-red transition duration-300' href="/connect">get in touch</a>
-              </li>
-              <li>
-                <Link className='text-the-red hover:text-the-off-white hover:bg-the-red transition duration-300' href="/about">about me</Link>
-              </li>
+            <div className="sm:hidden flex items-center">
+                <button
+                    onClick={toggleNavbar}
+                    className="text-the-red focus:outline-none mr-2">
+                    {isOpen ? ('x') : ('+')}
+                </button>
+            </div>
+
+            <ul className={`sm:flex items-center ${isOpen ? 'block' : 'hidden'}`}>
+                <li>
+                    <a className='text-the-red mr-6 hover:text-the-off-white hover:bg-the-red transition duration-300' href="#">resume</a>
+                </li>
+                <li>
+                    <a className='text-the-red mr-6 hover:text-the-off-white hover:bg-the-red transition duration-300' href="/connect">connect</a>
+                </li>
+                <li>
+                    <Link className='text-the-red hover:text-the-off-white hover:bg-the-red transition duration-300' href="/about">about</Link>
+                </li>
             </ul>
           </nav>
     );
