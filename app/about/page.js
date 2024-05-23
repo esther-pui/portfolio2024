6286a9cf71c098d86048e329953910926294c124
esther <estherpuihh@gmail.com>
2024-05-23 10:00:15 +0800

    adjust ui AND add more navigation to mobile navbar

--- app/about/page.js
+++ app/about/page.js
@@ -23,25 +23,24 @@ export default function About() {
           <Navbar/>
         </section>
 
-        <div className='relative mb-10 lg:flex-1 lg:px-20 flex flex-col items-center justify-center'>
-          <section className='relative z-10 max-w-screen-md mx-auto flex flex-col lg:flex-row items-center'>
+        <div className='relative z-10 mb-10 lg:flex-1 lg:px-20 flex flex-col items-center justify-center'>
+          <section className='max-w-screen-md flex flex-col lg:flex-row lg:items-stretch'>
             <div className='lg:order-1 mb-10'>
-              <div className='relative bg-the-red rounded-full w-60 h-60 overflow-hidden lg:flex-shrink-0 lg:w-80 lg:h-80'>
+              <div className='relative bg-the-red rounded-full lg:rounded-3xl w-60 h-60 overflow-hidden mx-auto lg:w-80 lg:h-[100%]'>
                 <Image src={avatar} alt="" layout="fill" objectFit="cover" />
               </div>
             </div>
 
             <div className='pb-10 lg:order-2 lg:ml-20 lg:flex-grow'>
               <p className="font-prata text-2xl lg:text-4xl text-the-red text-center lg:text-left">Esther Pui</p>
-              <p className="text-lg text-the-red text-center md:text-left">Full-stack developer</p>
-              <p className="text-the-red text-center mb-10 md:text-left">(React, Laravel)</p>
-              <p className="text-the-red text-center md:text-left">
+              <p className="text-lg text-the-red text-center lg:text-left">Full-stack developer</p>
+              <p className="text-the-red text-center mb-10 lg:text-left">(React, Laravel)</p>
+              <p className="text-the-red text-center lg:text-left">
                 Proud Sarawakian currently living in Kuala Lumpur, Malaysia.
                 My main focus these days is seamlessly integrating both backend and frontend elements based on UI design.
                 <br></br>
                 <br></br>
                 During my freetime, I like to practice in 3d, motion (three.js, webgl, blender) and digital illustration.
-
                 <br></br>
                 <br></br>
                 When I’m not at the computer,
@@ -50,7 +49,6 @@ export default function About() {
             </div>
           </section>
         </div>
-
       </div>
       <Footer/>
     </main>
--- app/mobileNav/page.js
+++ app/mobileNav/page.js
@@ -9,12 +9,12 @@ export default function MobileNav() {
         <main className='min-h-screen flex flex-col bg-the-off-white'>
             <div className='absolute inset-0 flex items-center justify-center'>
                 <div className='
-                circlePosition w-[80%] h-[70%]
-                bg-the-red opacity-30 rounded-[100%] 
-                fixed top-[50%] left-[50%] 
-                translate-x-[-50%] translate-y-[-50%]
-                blur-[200px]
-                z-1'
+                    circlePosition w-[80%] h-[70%]
+                    bg-the-red opacity-30 rounded-[100%] 
+                    fixed top-[50%] left-[50%] 
+                    translate-x-[-50%] translate-y-[-50%]
+                    blur-[200px]
+                    z-1'
                 ></div>
             </div>
             <div className='relative mt-40 mb-6 flex justify-center items-center'>
@@ -22,27 +22,29 @@ export default function MobileNav() {
                     className="text-the-red text-3xl focus:outline-none flex"
                     onClick={() =>  router.back()}
                     style={{ width: '2rem', height: '2rem', borderRadius: '50%' }}
-                >
-                    X
-                </button>
+                >X</button>
             </div>
-            <div className='relative z-10 mx-8 lg:px-20'>
+            <div className='relative z-10 mx-20 lg:px-20'>
                 <div className="border-t border-the-red mb-3 lg:w-1/2 mx-auto"></div>
                 <div className="flex flex-col lg:justify-center lg:flex-row">
                     <Link 
-                        className='text-4xl italic font-prata text-the-red text-center lg:mx-4' 
+                        className='text-4xl mb-2 italic font-prata text-the-red text-center lg:mx-4' 
                         href="/"
                     >HOME</Link>
                     <Link 
-                        className='text-4xl italic font-prata text-the-red text-center lg:mx-4' 
+                        className='text-4xl mb-2 italic font-prata text-the-red text-center lg:mx-4' 
+                        href="/portfolio"
+                    >PORTFOLIO</Link>
+                    <Link 
+                        className='text-4xl mb-2 italic font-prata text-the-red text-center lg:mx-4' 
                         href="#"
                     >RESUME</Link>
                     <Link 
-                        className='text-4xl italic font-prata text-the-red text-center lg:mx-4' 
+                        className='text-4xl mb-2 italic font-prata text-the-red text-center lg:mx-4' 
                         href="/connect"
                     >CONNECT</Link>
                     <Link 
-                    className='text-4xl italic font-prata text-the-red text-center lg:mx-4' 
+                    className='text-4xl mb-2 italic font-prata text-the-red text-center lg:mx-4' 
                     href="/about"
                     >ABOUT</Link>
                 </div>
