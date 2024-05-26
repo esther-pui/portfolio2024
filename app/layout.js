c32749cb1924b0d677214a9fe8dad696fe565068
esther <estherpuihh@gmail.com>
2024-05-26 20:09:22 +0800

    add google tag (Testing)

--- app/layout.js
+++ app/layout.js
@@ -22,18 +22,23 @@ export default function RootLayout({ children }) {
           `
         }} />
 
+      <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WHRBDZZ8'); 
+            `
+      }} />
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4F9MCSZJ29"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-4F9MCSZJ29');`
-        }} />
       </head>
-      <body className={outfit.className}>{children}</body>
+      <body className={outfit.className}>
+      
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHRBDZZ8"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
+        {children}
+</body>
     </html>
   );
 }
